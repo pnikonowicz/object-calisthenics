c986fa0b1e65ff6fa6747b4204da49d41d0b8a7b
Paul Nikonowicz <pnikonowicz@cyrusinnovation.com>
2013-04-11 13:31:03 -0400

    jobseeker can save jobs

---
+++ src/Jobseeker.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pnikonowicz
+ * Date: 4/11/13
+ * Time: 1:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Jobseeker {
+    public void save(Job job) {
+
+    }
+}
