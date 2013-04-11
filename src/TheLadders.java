6a95bc735b9013a7bfcf9ae31ca9b34d2b27ffc7
Paul Nikonowicz <pnikonowicz@cyrusinnovation.com>
2013-04-11 13:57:29 -0400

    the ladders should be able to see jobseekers who have applied to jobs

---
+++ src/TheLadders.java
@@ -0,0 +1,15 @@
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pnikonowicz
+ * Date: 4/11/13
+ * Time: 1:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TheLadders {
+    public Collection<Jobseeker> whoAppliedToJobOn(Date date) {
+        return null;
+    }
+}
