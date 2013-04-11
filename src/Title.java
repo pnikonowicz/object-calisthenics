e042c359ff21fd93d050963ce1e6a4fcaca611ca
Paul Nikonowicz <pnikonowicz@cyrusinnovation.com>
2013-04-11 16:17:20 -0400

    extracted title

--- src/Title.java
+++ src/Title.java
@@ -2,18 +2,17 @@
  * Created with IntelliJ IDEA.
  * User: pnikonowicz
  * Date: 4/11/13
- * Time: 1:35 PM
+ * Time: 4:14 PM
  * To change this template use File | Settings | File Templates.
  */
-public class ATS implements Job  {
-    private Title title;
+public class Title {
+    private String name;
 
-    public Application apply() {
-        return null;
+    public Title(String name) {
+        this.name = name;
     }
 
-    @Override
     public String toString() {
-        return title.toString();
+        return name;
     }
 }
