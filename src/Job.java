6f2d64c0a36597dde9e1340e76cc7565a2a065fa
Paul Nikonowicz <pnikonowicz@cyrusinnovation.com>
2013-04-11 13:28:43 -0400

    recruiters can post jobs

---
+++ src/Job.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pnikonowicz
+ * Date: 4/11/13
+ * Time: 1:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Job {
+}
---
+++ src/Recruiter.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pnikonowicz
+ * Date: 4/11/13
+ * Time: 1:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Recruiter {
+    public void post(Job job) {
+
+    }
+}
