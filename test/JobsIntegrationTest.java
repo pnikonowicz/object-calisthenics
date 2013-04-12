80639f526bffac63dd6581c8580077e037aa3803
Paul Nikonowicz <pnikonowicz@cyrusinnovation.com>
2013-04-12 16:04:35 -0400

    Jobs, when displayed, should be displayed with a title.

---
+++ test/JobsIntegrationTest.java
@@ -0,0 +1,26 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.StringWriter;
+import java.io.Writer;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pnikonowicz
+ * Date: 4/12/13
+ * Time: 4:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JobsIntegrationTest {
+    @Test
+    public void jobsWhenDisplayedShouldBeDisplayedWithTitle() {
+        Job job = new ATS(mock(Recruiter.class), new Title("title"));
+        Writer writer = new StringWriter();
+
+        job.displayTitle(writer);
+
+        Assert.assertEquals("title", writer.toString());
+    }
+}
