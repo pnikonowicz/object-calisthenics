0ba3e5ee74186f3efb2fec2cd2f7f7edc0a9e73e
Paul Nikonowicz <pnikonowicz@cyrusinnovation.com>
2013-04-12 17:38:43 -0400

    make test more readable

--- test/TheLaddersIntegrationTest.java
+++ test/TheLaddersIntegrationTest.java
@@ -37,13 +37,13 @@ public class TheLaddersIntegrationTest {
     @Test
     public void theLaddersShouldBeAbleToSeeAggregateJobApplicationNumbersByJobAndRecruiter() {
         ApplicationRepository applicationRepository = new ApplicationRepository();
-        JobSeeker jobSeeker_A = new JobSeeker(new Resume(new Name("resume a")), new Name("job seeker a"));
-        JobSeeker jobSeeker_B = new JobSeeker(new Resume(new Name("resume b")), new Name("job seeker b"));
-        Recruiter recruiter_A = new Recruiter(new Name("recruiter a"));
-        Recruiter recruiter_B = new Recruiter(new Name("recruiter b"));
-        Job job_a = new JReq(recruiter_A, new Title("job a"));
-        Job job_b = new JReq(recruiter_B, new Title("job b"));
-        Job job_c = new JReq(recruiter_A, new Title("job c"));
+        JobSeeker jobSeeker_A = new JobSeeker(mock(Resume.class), mock(Name.class));
+        JobSeeker jobSeeker_B = new JobSeeker(mock(Resume.class), mock(Name.class));
+        Recruiter recruiter_A = mock(Recruiter.class);
+        Recruiter recruiter_B = mock(Recruiter.class);
+        Job job_a = new JReq(recruiter_A, mock(Title.class));
+        Job job_b = new JReq(recruiter_B, mock(Title.class));
+        Job job_c = new JReq(recruiter_A, mock(Title.class));
         TheLadders theLadders = new TheLadders();
         Writer writer = new StringWriter();
 
@@ -51,6 +51,7 @@ public class TheLaddersIntegrationTest {
         jobSeeker_B.apply(job_b, applicationRepository, new ApplicationNumber(2));
         jobSeeker_A.apply(job_b, applicationRepository, new ApplicationNumber(3));
         jobSeeker_B.apply(job_a, applicationRepository, new ApplicationNumber(4));
+        jobSeeker_A.apply(job_c, applicationRepository, new ApplicationNumber(4));
 
         theLadders.displayJobApplicationNumbersForThisRecruiterAndJob(writer, job_a, recruiter_A, applicationRepository);
 
